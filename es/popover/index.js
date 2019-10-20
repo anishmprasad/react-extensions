c372f620ce6d81b644cb36a5548fe8d998ce5cc5
Anish.M.Prasad <anish.m.prasad@gmail.com>
2019-10-20 19:02:22 +0530

    popover added

---
+++ es/popover/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Rpopover from 'rpopover';
+export default function Popover(props) {
+  return React.createElement(Rpopover, props);
+}
+//# sourceMappingURL=index.js.map
